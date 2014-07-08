d73606be3310604934ef760f37b8c136069ab4f1
LGloveyuan <747904126@qq.com>
2014-07-08 22:06:07 +0800

    Update Wrapper_gjsairen001.java

--- src/Wrapper_gjsairen001.java
+++ src/Wrapper_gjsairen001.java
@@ -360,7 +360,12 @@ public class Wrapper_gjsairen001 implements QunarCrawler {
 	        	
 	        	flightDetail.setDepcity(searchParam.getDep());
 		        flightDetail.setArrcity(searchParam.getArr());
-		        flightDetail.setDepdate(forMatDate(searchParam.getDepDate()));
+		        if(0 == flag){
+		        	flightDetail.setDepdate(forMatDate(searchParam.getDepDate()));	
+		        }
+		        else if(1 == flag){
+		        	flightDetail.setDepdate(forMatDate(searchParam.getRetDate()));
+		        }
 		        flightDetail.setFlightno(flightNos);
 		        flightDetail.setMonetaryunit(monUnit);
 		        flightDetail.setTax(0);
